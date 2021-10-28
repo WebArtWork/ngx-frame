ac47143ce9db8f72dc74ccb7474c506445752ea2
Honchar Denys <crackeraki@gmail.com>
2021-10-29 01:47:26 +0300

    initialize

---
+++ frame.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+	selector: 'frame',
+	templateUrl: './frame.component.html',
+	styleUrls: ['./frame.component.scss']
+})
+export class FrameComponent{
+	constructor() {}
+}
---
+++ frame.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CoreModule } from '@core';
+import { FrameComponent } from './frame.component';
+
+@NgModule({
+	imports: [
+		CoreModule
+	],
+	declarations: [
+		FrameComponent
+	],
+	providers: []
+
+})
+
+export class FrameModule { }
